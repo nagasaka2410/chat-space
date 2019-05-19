21240447c569d5ceae3eac6b18d20557efa3c8a8
nagasaka2410 <warkun20@gmail.com>
2019-05-19 20:42:38 +0900

    edit_javascript
    javascriptの編集

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,15 +1,13 @@
 $(function(){
   function buildHTML(message){
-
-    message.image? image = '<img class="lower-message__image" src=${message.image}>' : image = "";
-
-    var html = `<div class="message">
+    var image = message.image.url ? `<img class="lower-message__image" src=${message.image}>` : "";
+    var html = `<div class="message" data-id= "${ message.id }">
                   <div class='section'>
                     <div class='section__user-name'>
-                    ${message.name}
+                    ${message.user_name}
                     </div>
                     <div class='section__date'>
-                    ${message.date}
+                    ${message.created_at}
                     </div>
                   </div>
                   <div class='lower-meesage'>
@@ -45,4 +43,32 @@ $(function(){
     })
     return false;
   })
+
+  var reloadMessages = function() {
+    last_message_id = $('.message:last').data('id');
+    var test_url = window.location.href.replace(/messages/,"");
+    var url = test_url + "api/messages";
+    if(url.match(/\/groups\/\d+\/api\/messages/)){
+      $.ajax({
+        url: url,
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        messages.forEach(function(message){
+          var insertHTML = buildHTML(message);
+          $(".messages").append(insertHTML);
+        })
+        $(".messages").animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      })
+      .fail(function() {
+        alert('自動更新に失敗しました');
+      });
+    } else {
+      clearInterval();
+    }
+  };
+  reloadMessages(); 
+  setInterval(reloadMessages, 5000);
 })
