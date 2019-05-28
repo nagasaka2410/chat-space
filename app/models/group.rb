ff3298a977e03eb9b5532be0c82fa5edaa15edae
nagasaka2410 <warkun20@gmail.com>
2019-05-28 21:13:51 +0900

    sub
    comitt

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,9 @@
 class Group < ApplicationRecord
+
   has_many :members
   has_many :users, through: :members
   has_many :messages
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 
   def show_last_message
     if (last_message = messages.last).present?
